cf9647796a5baf06c5e0a136e896a2ca217953ac
Oleg <100606637+OlegEco@users.noreply.github.com>
2024-08-22 18:01:23 +0200

    Add project files.

---
+++ Add Two Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+Solution sol = new Solution();
+var result = sol.AddTwoNumbers(l1, l2);
+
+Console.WriteLine("Result: ");
+while (result != null)
+{
+    Console.Write(result.val);
+    if (result.next != null) Console.Write(" -> ");
+    result = result.next;
+}
+
+
